22412aeb549a1e9d2d61ce1d24d716dd443f3f36
Mervlim <melzlim@hotmail.com>
2017-07-27 22:51:15 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import Debug from 'debug';
+import express from 'express';
+import logger from 'morgan';
+// import favicon from 'serve-favicon';
+import path from 'path';
+import lessMiddleware from 'less-middleware';
+import index from './routes/index';
+import mongoose from 'mongoose';
+import alphavantageAPI from './routes/alphaVantage';
+
+
+const app = express();
+const debug = Debug('fin-tail-backend:app');
+
+
+
+// Connect to mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Fintail');
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/apiAlphaVantage', alphavantageAPI);
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+/* eslint no-unused-vars: 0 */
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Handle uncaughtException
+process.on('uncaughtException', (err) => {
+  debug('Caught exception: %j', err);
+  process.exit(1);
+});
+
+export default app;
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+/* GET index page. */
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+
+export default router;
